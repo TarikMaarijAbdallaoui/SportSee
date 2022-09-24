943e80fc7c4ecf18014edc84fc42719bc9df0e5e
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-09-24 16:26:40 +0200

    Creation of the HEADER with the styles

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Logo from '../Logo'
+import Menu from '../Menu'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+        <Logo />
+        <Menu />
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Menu.css'
+
+const Menu = () => {
+  return (
+    <nav>
+        <ul>
+                <li>Accueil</li>
+                <li>Profil</li>
+                <li>Réglage</li>
+                <li>Communauté</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Menu
