2bc0e99e3c844a9c7fdee50eaa6b30d61763ddc7
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-10-22 14:25:51 +0200

    User path definition

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-const application = document.getElementById('root')
-const root = ReactDOM.createRoot(application)
+const application = document.getElementById("root");
+const root = ReactDOM.createRoot(application);
 
-root.render(<App />)
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/:id" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
