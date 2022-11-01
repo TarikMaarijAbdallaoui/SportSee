09d3cb178398daff79e9b1dc67300a9855f6d2e7
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-11-01 14:07:46 +0100

    API access service

---
+++ src/getData/index.jsx
@@ -0,0 +1,49 @@
+const BASE_URL = "http://localhost:3000/user"
+
+export async function getData(id) {
+  try {
+    const response = await fetch(`${BASE_URL}/${id}`);
+    const data = await response.json();
+
+    console.log(data.data);
+    return data.data
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getPerformance(id) {
+  try {
+    const response = await fetch(`${BASE_URL}/${id}/performance`);
+    const data = await response.json();
+
+    console.log(data.data);
+    return data.data
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getActivity(id) {
+  try {
+    const response = await fetch(`${BASE_URL}/${id}/activity`);
+    const data = await response.json();
+
+    console.log("get Activity", data.data.sessions);
+    return data.data.sessions
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getAverageSessions(id) {
+  try {
+    const response = await fetch(`${BASE_URL}/${id}/average-sessions`);
+    const data = await response.json();
+
+    console.log(data.data);
+    return data.data
+  } catch (error) {
+    console.log(error);
+  }
+}
