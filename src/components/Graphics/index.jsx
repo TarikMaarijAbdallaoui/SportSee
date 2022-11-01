6c2f80f8a1343ad4285df32c62ba2ace9c4c4656
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-11-01 14:09:03 +0100

    graphics layout

--- src/components/Graphics/index.jsx
+++ src/components/Graphics/index.jsx
@@ -1,28 +1,47 @@
-import React from "react";
-import BarChart from "../BarChart";
+import React, { useState, useEffect } from "react";
 import RadarGraph from "../RadarGraph";
 import AverageSessions from "../AverageSessions";
 import "./Graphics.css";
-import Lipids from "../Lipids";
-import Proteins from "../Proteins";
-import Carbs from "../Carbs";
-import Calories from "../Calories";
+import Lipids from "../KeyData/Lipids";
+import Proteins from "../KeyData/Proteins";
+import Carbs from "../KeyData/Carbs";
+import Calories from "../KeyData/Calories";
+import { getData } from "../../getData";
+import { useParams } from "react-router-dom";
+import KPI from "../KPI";
+import Activity from "../BarChart";
 
 const Graphics = () => {
+  const [user, setUser] = useState({})
+  const { id } = useParams();
+
+  useEffect(()=>{
+    async function fetchUser(id) {
+      const userData = await getData(id);
+      setUser(userData)
+    }
+
+    fetchUser(id)
+  }, [])
+  
   return (
     <div className="graphics_container">
       <div className="graphics">
-        <BarChart />
+        <Activity />
         <div className="radar">
+          {/* <div className="average"> */}
           <AverageSessions />
-          <RadarGraph />
+          {/* </div> */}
+          <div className="radarg">
+          <RadarGraph /></div>
+          <KPI />
         </div>
       </div>
       <div className="stats">
-        <Calories data={100} />
-        <Proteins data={100}/>
-        <Carbs data={100} />
-        <Lipids data={100} />
+        <Calories data={user?.keyData?.calorieCount || 100} />
+        <Proteins data={user?.keyData?.proteinCount || 100} />
+        <Carbs data={user?.keyData?.carbohydrateCount || 100} />
+        <Lipids data={user?.keyData?.lipidCount ||100} />
       </div>
     </div>
   );
