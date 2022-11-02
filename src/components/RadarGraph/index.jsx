3c944e2077ea88800b1db64c9848217c90565deb
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-11-02 13:50:11 +0100

    radar chart style

--- src/components/RadarGraph/index.jsx
+++ src/components/RadarGraph/index.jsx
@@ -1,70 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
-import { getPerformance } from '../../getData';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  ResponsiveContainer,
+} from "recharts";
+import { getPerformance } from "../../getData";
 
-const data = [
-  {
-    subject: 'Math',
-    A: 120,
-    B: 110,
-    fullMark: 150,
-  },
-  {
-    subject: 'Chinese',
-    A: 98,
-    B: 130,
-    fullMark: 150,
-  },
-  {
-    subject: 'English',
-    A: 86,
-    B: 130,
-    fullMark: 150,
-  },
-  {
-    subject: 'Geography',
-    A: 99,
-    B: 100,
-    fullMark: 150,
-  },
-  {
-    subject: 'Physics',
-    A: 85,
-    B: 90,
-    fullMark: 150,
-  },
-  {
-    subject: 'History',
-    A: 65,
-    B: 85,
-    fullMark: 150,
-  },
-];
 
 export default function RadarGraph() {
-  const [performance, setPerformance] = useState({})
-  const {id} = useParams()
+  const [performance, setPerformance] = useState({});
+  const { id } = useParams();
   useEffect(() => {
     async function fetchData() {
-      const data = await getPerformance(id)
+      const data = await getPerformance(id);
 
-      setPerformance(data)
+      setPerformance(data);
     }
-    fetchData()
-  }, [])
-  
-  console.log(performance)
- 
-    return (
-      <ResponsiveContainer width="100%" height="100%">
-        <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
-          <PolarGrid />
-          <PolarAngleAxis dataKey="subject" />
-          <PolarRadiusAxis />
-          <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
-        </RadarChart>
-      </ResponsiveContainer>
-    );
-  
+    fetchData();
+  }, []);
+
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <RadarChart cx="48%" cy="50%" outerRadius="70%" data={performance}>
+        <PolarGrid radialLines={false}/>
+        <PolarAngleAxis
+          dataKey="kind"
+          tickLine={false}
+          stroke="#fff"
+          tickSize={10}
+          axisLine={false}
+          tick={{ scaleToFit: true, fontSize: 10 }}
+        />
+        <Radar
+          name="kind"
+          dataKey="value"
+          stroke="#FF0101"
+          fill="#FF0101"
+          fillOpacity={0.7}
+        />
+      </RadarChart>
+    </ResponsiveContainer>
+  );
 }
