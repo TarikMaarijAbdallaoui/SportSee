038ec0f47e9893ebb3827e3f0b43884bc02f3e65
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-11-02 13:07:04 +0100

    sessions component

--- src/components/AverageSessions/index.jsx
+++ src/components/AverageSessions/index.jsx
@@ -14,50 +14,15 @@ import {
 } from "recharts";
 import "./AverageSessions.css"
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+const CustomTooltip = ({active, payload}) => {
+  if (active && payload) {
+    return (
+      <div className="tooltip">
+        <p>{`${payload[0].value}`}min</p>
+      </div>
+    )
+  }
+}
 
 export default function AverageSessions() {
   const [average, setAverage] = useState({});
@@ -66,11 +31,7 @@ export default function AverageSessions() {
   useEffect(() => {
     async function fetchUser(id) {
       const userAverageSessions = await getAverageSessions(id);
-      const days = ["L", "M", "M", "J", "V", "S", "D"];
-      userAverageSessions.sessions.forEach(
-        (element, index) => (element.day = days[index])
-      );
-      setAverage(userAverageSessions.sessions);
+      setAverage(userAverageSessions);
     }
 
     fetchUser(id);
@@ -83,18 +44,18 @@ export default function AverageSessions() {
     <div className="average">
       <p className="title">Durée moyenne des sessions</p>
       <ResponsiveContainer width="100%" height="100%">
-        <LineChart width={500} height={300} data={data} margin={{top: 120, bottom: 30}}>
-          {/* <CartesianGrid strokeDasharray="3 3" /> */}
-          <XAxis dataKey="day" tickLine={false} />
-          {/* <YAxis /> */}
-          <Tooltip />
-          {/* <Legend /> */}
-          {/* <Line type="monotone" dataKey="sessionLength" stroke="#8884d8" activeDot={{ r: 8 }} /> */}
+        <LineChart data={data} margin={{top: 20, right: 20, left: -40, bottom: 5}}>
+          <XAxis dataKey="day" tickLine={false} axisLine={false} stroke="white" />
+          <YAxis tickLine={false} axisLine={false} tick={false} domain={["dataMin - 10", "dataMax + 10"]} />
+          <Tooltip content={<CustomTooltip />} />
           <Line
             type="monotone"
             dataKey="sessionLength"
+            strokeWidth={2}
             stroke="#FFFFFF"
             activeDot={{ r: 8 }}
+            dot={false}
+            
           />
         </LineChart>
       </ResponsiveContainer>
